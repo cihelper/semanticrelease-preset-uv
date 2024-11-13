c52f69acb7a4f5986efecd5c4c9bdccffc693956
Mirko Lenz <mirko@mirkolenz.com>
2024-11-13 08:17:41 +0100

    feat: initial commit

---
+++ .releaserc.js
@@ -0,0 +1,62 @@
+export default {
+  // https://semantic-release.gitbook.io/semantic-release/usage/workflow-configuration
+  branches: [
+    { name: "main" },
+    { name: "next" },
+    { name: "+([0-9])?(.{+([0-9]),x}).x" },
+    { name: "dev", prerelease: true },
+    { name: "beta", prerelease: true },
+    { name: "alpha", prerelease: true },
+  ],
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+      },
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalcommits",
+      },
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogTitle: "# Changelog",
+      },
+    ],
+    [
+      "@cihelper/semanticrelease-plugin-uv",
+      {
+        uvPublish: process.env.SEMREL_UV_PUBLISH !== "false",
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          { path: "dist/*.tar.gz", label: "sdist" },
+          { path: "dist/*.whl", label: "wheel" },
+        ],
+        failComment: false,
+        successComment: false,
+        addReleases: "bottom",
+      },
+    ],
+    [
+      "semantic-release-major-tag",
+      {
+        customTags: ["v${major}", "v${major}.${minor}"],
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        message: "chore(release): ${nextRelease.version}",
+        assets: ["pyproject.toml", "*/__init__.py", "CHANGELOG.md"],
+      },
+    ],
+  ],
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# CI Preset for Semantic Release with uv
+
+A shareable semantic-release configuration for Python packages that use uv.
